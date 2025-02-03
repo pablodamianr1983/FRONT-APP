c409a3184576c48b3e89fe73cf003cc1b83874d8
200_de_salame <122568719+pablodamianr1983@users.noreply.github.com>
2025-02-03 12:19:38 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -19,7 +19,7 @@ api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
     if (token) {
-      config.headers["Authorization"] = Bearer ${token}; // ⬅️ Corrección: Usa "Authorization" estándar
+      config.headers["Authorization"] = `Bearer ${token}`; // ⬅️ Corrección: Usa "Authorization" estándar
     }
     return config;
   },
